c04e3e77f8ef6283cbf06c91127cad39cd84055c
Lidia A <lidia.lasy@gmail.com>
2023-04-11 19:01:43 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.scss'
+
+import Board from './components/Board'
+
+function App() {
+
+  return (
+    <>
+      <h1>This is a TicTacToe game</h1>
+      <Board />
+    </>
+  )
+}
+
+export default App
