f4d1f581a860c587f438f18927329ecc39a9189c
Lidia A <lidia.lasy@gmail.com>
2023-04-22 12:43:26 +0200

    Add game function complete

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,14 +1,5 @@
-import { useState } from 'react';
-
-export default function Square() {
-    const [value, setValue] = useState(null);
-
-    function handleClick() {
-        console.log('Is clicked!');
-        setValue('X');
-    }
-
+export default function Square(props) {
     return (
-        <button className="square" onClick={handleClick}>{value}</button>
+        <button className="square" onClick={props.onSquareClick}>{props.value}</button>
     );
   }
